a4837bcec894b8e84cad630cdc1b805fe1703341
夭暝 <@DESKTOP-01RB0UP>
2020-04-08 23:13:54 +0800

    Add pages for reader.

--- routes/reader.js
+++ routes/reader.js
@@ -45,21 +45,21 @@ router.get('/', function (req, res) {
 
 router.get('/reader/:id', async function (req, res) {
     //await 异步执行
-    let reader = await userController.getReaderInfoById(req.params.id)
+    let reader = await readerlogic.getReaderInfoById(req.params.id)
     return res.json({ code: 0, msg: '', data: reader });
 })
 
 router.put('/reader/:id', async function (req, res) {
     const { name, email } = req.body;
     const id = req.params.id
-    let result = await userController.updateReaderInfo({ name, email, id })
+    let result = await readerlogic.updateReaderInfo({ name, email, id })
     return res.json({ code: 0, msg: '修改成功', data: result });
 })
 
 router.post('/changepass/:id', async function (req, res) {
     const { pass } = req.body;
     const id = req.params.id
-    let reader = await userController.changePass(pass, id)
+    let reader = await readerlogic.changePass(pass, id)
     return res.json({ code: 0, msg: '修改成功', data: reader });
 })
 module.exports = router;
